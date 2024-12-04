90fd1ab1eb4e6b67411925ed3b6f647fd16a24d1
Harsh Dhobe <harshdhobe123@gmail.com>
2024-12-04 23:09:54 +0530

    updates

--- project1/index.js
+++ project1/index.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const app = express();
 const users = require("./MOCK_DATA.json");
+const fs = require("fs");
 const PORT = 8001;
 
 app.get("/api/users", (req, res) => {
     return res.json(users);
 });
+
+
+app.post("/api/users", (req, res) => {
+    const body = req.body;
+    users.push({ body, id: users.length + 1 });
+    fs.writeFile("./MOCK_DATA.json", (err, data) => {
+        return res.json({ status: "pending" })
+    })
+
+
+});
+
 
 //dynamic id 
 app.get("/api/users/:id", (req, res) => {
